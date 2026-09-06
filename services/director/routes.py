8638bd5f3551e42fcc9b6faa83394075049daf71
Marko Mijanovic <markomijanovic@users.noreply.github.com>
2026-09-07 01:05:59 +0200

    Add director investment report

--- services/director/routes.py
+++ services/director/routes.py
@@ -39,3 +39,85 @@ def pending_orders():
     ]
 
     return jsonify(orders=orders),200
+
+@director_blueprint.get("/report")
+@role_required("director")
+def report():
+    mongo_database = current_app.extensions[
+        "mongo_database"
+    ]
+
+    assets_collection = mongo_database[
+        "assets"
+    ]
+
+    pipeline = [
+        {
+            "$unwind": "$categories"
+        },
+        {
+            "$group": {
+                "_id": "$categories",
+                "spent": {
+                    "$sum": "$buying_price"
+                },
+                "earned": {
+                    "$sum": {
+                        "$cond": [
+                            {
+                                "$and": [
+                                    {
+                                        "$ne": [
+                                            {
+                                                "$type": (
+                                                    "$selling_price"
+                                                )
+                                            },
+                                            "missing",
+                                        ]
+                                    },
+                                    {
+                                        "$ne": [
+                                            {
+                                                "$type": (
+                                                    "$selling_date"
+                                                )
+                                            },
+                                            "missing",
+                                        ]
+                                    },
+                                ]
+                            },
+                            "$selling_price",
+                            0,
+                        ]
+                    }
+                },
+            }
+        },
+        {
+            "$project": {
+                "_id": 0,
+                "category": "$_id",
+                "spent": 1,
+                "earned": 1,
+            }
+        },
+        {
+            "$sort": {
+                "earned": -1,
+                "spent": 1,
+                "category": 1,
+            }
+        },
+    ]
+
+    statistics = list(
+        assets_collection.aggregate(
+            pipeline
+        )
+    )
+
+    return jsonify(
+        statistics=statistics
+    ), 200
